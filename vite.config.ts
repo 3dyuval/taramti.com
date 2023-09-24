359d68cd90a53ce14d3c6c78455e65de944dd9c7
3dyuval <3dyuval@gmail.com>
2023-09-25 01:05:38 +0300

    added dependency

--- vite.config.ts
+++ vite.config.ts
@@ -16,13 +16,8 @@ export default defineConfig({
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
   },
-  ssr: { noExternal: ['primevue', '@fawmi/vue-google-maps', 'vuetify'] },
+  ssr: { noExternal: ['primevue', '@fawmi/vue-google-maps', 'vuetify', '@googlemaps/markerclusterer'] },
   build: {
     emptyOutDir: true,
-  },
-  optimizeDeps: {
-    include: ['@fawmi/vue-google-maps', 'fast-deep-equal'],
-    //    disabled: false,
-    //    force: true,
   },
 }) satisfies UserConfigExport
