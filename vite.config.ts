9289a2c5249ec2fa93c63aa3d195c9a508b07c8e
Yuval Dikerman <3dyuval@gmail.com>
2023-09-24 11:54:54 +0300

    update to vike

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import type { UserConfigExport } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import * as path from 'path'
-import ssr from 'vite-plugin-ssr/plugin'
+import ssr from 'vike/plugin'
 import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
 
 
