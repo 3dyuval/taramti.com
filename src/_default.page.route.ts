a59a11920f28f400f4b707e1ceb9391d1b01baea
3dyuval <3dyuval@gmail.com>
2023-12-01 18:34:43 +0200

    add trailingSlash to URL root

--- src/_default.page.route.ts
+++ src/_default.page.route.ts
@@ -19,7 +19,7 @@ function onBeforeRoute(pageContext: PageContextServer) {
 
   return {
     pageContext: {
-      urlLogical: pageContext.urlOriginal.replace(`/${locale}`, '/'),
+      urlLogical: pageContext.urlOriginal.replace(`/${locale}`, '') || '/',
       locale
     }
   }
