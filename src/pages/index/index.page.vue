f32b9b9b2d422e9d59a7d5baf862b30261f308da
3dyuval <3dyuval@gmail.com>
2023-12-16 18:10:52 +0200

    added background images slideshow

--- src/pages/index/index.page.vue
+++ src/pages/index/index.page.vue
@@ -1,23 +1,73 @@
 <script setup lang='ts'>
 import SearchCard from '@/components/SearchCard.vue'
 import type { DonationLocationDate } from '@/types'
+import { createClient } from 'pexels'
+import { onMounted, ref } from 'vue'
 
 const props = defineProps<{
   locations: DonationLocationDate[]
 }>()
 
-</script>
+const client = createClient(import.meta.env['VITE_PEXELS_API_KEY'])
+const images = ref([])
 
+onMounted(async () => {
+  const query = 'blood donation'
+  const response = await client.photos.search({ query, per_page: 5 })
+  images.value = response.photos.map(photo => photo.src.landscape)
+})
+
+
+</script>
 <template>
-  <div class='index container'>
-    <search-card v-bind='props' class='card-center' />
-  </div>
+  <v-carousel full-screen class='fullscreen-carousel' hide-delimiters
+              selected-class='active-slide'
+              :show-arrows='false' continuous
+              :interval='4000' cycle :direction='$t("settings.dir")'>
+    <v-carousel-item v-for='(image, i) in images' :key='i'>
+      <v-img :src='image' cover></v-img>
+    </v-carousel-item>
+  </v-carousel>
+  <search-card v-bind='props' class='card-center' />
 </template>
 
 <style lang='scss'>
 
+
+.fullscreen-carousel {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh !important;
+  z-index: 0;
+
+  .v-responsive.v-img {
+    height: 100%;
+
+  }
+
+  .v-window-item {
+    &.active-slide {
+      animation: blur 3s;
+      animation-fill-mode: forwards;
+      animation-iteration-count: 1;
+      animation-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
+    }
+  }
+}
+
+@keyframes blur {
+  0% {
+    filter: blur(15px);
+  }
+  100% {
+    filter: blur(5px);
+  }
+}
+
 .index.container {
-  height: 100lvmin;
+  height: 100vh;
   display: flex;
   flex-direction: column;
   justify-content: center;
@@ -25,7 +75,12 @@ const props = defineProps<{
 }
 
 .card-center {
-  margin: 0 auto;
+  padding: 1rem;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  z-index: 10;
   width: 50%;
 }
 </style>
